540d8776ef968fc607012d7949e443a35d73562f
artdevil <artdvl91@gmail.com>
2013-02-11 20:22:14 +0700

    fix for notification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
     @current_ability ||= Ability.new(current_user)
   end
   
-  def create_notification sender_id, receiver_id, notifiable_id, notifiable_type, status_id, status_description
-    notification = Notification.new(:sender_id => sender_id, :receiver_id => receiver_id, :notifiable_id => notifiable_id, :notifiable_type => notifiable_type, :status_id => status_id, :status_description => status_description)
+  def create_notification sender_id, receiver_id, notifiable_id, notifiable_type, status_description
+    notification = Notification.new(:sender_id => sender_id, :receiver_id => receiver_id, :notifiable_id => notifiable_id, :notifiable_type => notifiable_type, :status_description => status_description)
     if notification.save
       return true
     else
