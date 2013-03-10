7b1466c62afdef4a09682a8aa84d69a8cfbcf40d
artdvl <artdvl91@gmail.com>
2013-03-10 18:49:53 +0700

    remove email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable, :registerable :recoverable,
-  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable, :authentication_keys => [:keyid]
+  devise :database_authenticatable, :registerable, :rememberable, :trackable
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me,:keyid, :name, :birthday, :gender, :religion,
+  attr_accessible :password, :password_confirmation, :remember_me, :keyid, :name, :birthday, :gender, :religion,
                   :avatar, :telephone, :user_type
-  
-  validates :email, :presence => true
   
   mount_uploader :avatar, AvatarUploader
   # attr_accessible :title, :body
@@ -25,7 +23,28 @@ class User < ActiveRecord::Base
   scope :user_student, lambda{|params| where("(name LIKE ? OR keyid LIKE ?) AND user_type != 'dosen'", "#{params}%","#{params}%")}
   scope :user_lecture, lambda{|params| where("(name LIKE ? OR keyid LIKE ?) AND user_type != 'mahasiswa'", "#{params}%","#{params}%")}
 
+  
+  
+  def email_required?
+    false
+  end
+  
   protected
+  
+  def self.included(base)
+    base.extend ClassMethods
+    assert_validations_api!(base)
+
+    base.class_eval do
+      validates_presence_of :email, :if => :email_required?
+      validates_uniqueness_of :email, :allow_blank => true, :if => :email_changed?, :scope => [:account]
+      validates_format_of :email, :with => email_regexp, :allow_blank => true, :if => :email_changed?
+      
+      validates_presence_of :password, :if => :password_required?
+      validates_confirmation_of :password, :if => :password_required?
+      validates_length_of :password, :within => password_length, :allow_blank => true
+    end
+  end
   
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130226115214) do
+ActiveRecord::Schema.define(:version => 20130304060800) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(:version => 20130226115214) do
   add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
   add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
   add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
+
+  create_table "admin_configs", :force => true do |t|
+    t.integer  "max_lecture_student_proposal"
+    t.integer  "max_lecture_student_scription"
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+  end
 
   create_table "admin_users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -170,7 +177,6 @@ ActiveRecord::Schema.define(:version => 20130226115214) do
   add_index "topics", ["slug"], :name => "index_topics_on_slug", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "email"
     t.string   "keyid",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
@@ -193,7 +199,7 @@ ActiveRecord::Schema.define(:version => 20130226115214) do
     t.string   "slug"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["keyid"], :name => "index_users_on_keyid", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["slug"], :name => "index_users_on_slug", :unique => true
 
